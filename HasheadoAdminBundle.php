ed24712539de2a1329b8a7e9ca399a9fad732fa2
Emiliano Viada <emjovi@gmail.com>
2014-10-30 00:44:40 -0300

    Overiding templates: Login page

--- HasheadoAdminBundle.php
+++ HasheadoAdminBundle.php
@@ -6,4 +6,11 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class HasheadoAdminBundle extends Bundle
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent()
+    {
+        return 'ApplicationSonataUserBundle';
+    }
 }
